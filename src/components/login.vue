4dc9fc013bf3f559a573e16882ebd45df74610f4
keaichong <844746224@qq.com>
2019-02-13 23:52:30 +0800

    引入登陆组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>login------</div>
+  <div class="login-warp">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="primary">主要按钮</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
