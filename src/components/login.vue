a57c29bca37d105a2559c04c197daec70b3214b2
keaichong <844746224@qq.com>
2019-02-14 15:14:24 +0800

    处理了登陆

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-warp">
+  <div class="login-wrap">
     <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
@@ -8,7 +8,7 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">主要按钮</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登陆</el-button>
     </el-form>
   </div>
 </template>
@@ -22,9 +22,53 @@ export default {
         password: ''
       }
     }
+  },
+  methods: {
+    handleLogin () {
+      this.$http
+        .post(`login`, this.formdata)
+        .then(res => {
+          // console.log(res)
+          const {
+            data: {
+              // data,
+              meta: { msg, status }
+            }
+          } = res
+          if (status === 200) {
+            this.$router.push({
+              name: 'home'
+            })
+          } else {
+            // element-ui插件的提示框
+            this.$message.error(msg)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
 
 <style>
+.login-wrap {
+  /* 注意: 百分比布局 父元素height:100% */
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  /* 开发 */
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/base.css'
 import router from './router'
-
+import Axios from 'axios'
+Axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
+Vue.prototype.$http = Axios
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
@@ -14,7 +17,9 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>' // vue1.0写法
 })
 // components: { App,App },
